c97fc3d8e58fc42d5843dc288b542a522b9afb16
ShnigdhaPaul <Shnigdha2000@gmail.com>
2021-06-13 15:42:14 +0600

    lab 3 added

--- PHP/Lab 3/problem 5.php
+++ PHP/Lab 3/problem 5.php
@@ -9,12 +9,15 @@ $imageHeight="80px";
 ?>
 
 <tr>
-			<td><img src="AIUB_logo.jpg" height="$imageHeight" Width="$imageWidth"></td>
-			<td><h2>American International <br>University -<span style="background-color:$backColor;color:$fontColor">Bangladesh</h2>
-</span>  </td>
+			<td><img src="AIUB_logo.jpg"  height=<?php echo"$imageHeight"?>; Width=<?php echo"$imageWidth"?>>   </td>
+			<td><h2>American International <br>University -<span style="background-color:<?php echo $backColor;?>; color:<?php echo "$fontColor"; ?>">Bangladesh</h2>
+ </td>
+			
 			</tr>
 
  
 </table>
+</span>
+
 </body>
 </html>
